b97a5ed0e3d8a5f6c06a99a05a31522c6a54db65
Mohammed Huraib <mhuraib@Mohammeds-iMac.local>
2016-07-01 08:51:09 +0300

    semi complete

--- little-monster/ViewController.swift
+++ little-monster/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var foodImg: DragImage!
     @IBOutlet weak var heartImg: DragImage!
     
+    let DIM_ALPHA : CGFloat = 0.2
+    let OPAQUE : CGFloat = 1.0
+    let MAX_LIVES = 3
+    var deaths = 0
+    var timer : NSTimer!
+    var monsterHappy = false
+    var currentItem: UInt32 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,12 +32,82 @@ class ViewController: UIViewController {
         foodImg.dropTarget = monsterImg
         heartImg.dropTarget = monsterImg
         
+        firstSkullImg.alpha = DIM_ALPHA
+        secondSkullImg.alpha = DIM_ALPHA
+        thirdSkullImg.alpha = DIM_ALPHA
+        startTimer()
+        
     }
     
     func itemDroppedOnCharacter (notify : AnyObject){
         
-        print("YEP")
+        monsterHappy = true
+        startTimer()
+        
+        foodImg.alpha = DIM_ALPHA
+        foodImg.userInteractionEnabled = false
+        
+        heartImg.alpha = DIM_ALPHA
+        heartImg.userInteractionEnabled = false
     }
+    
+    func startTimer(){
+        if timer != nil{
+            timer.invalidate()
+        }
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil , repeats: true)
+    }
+    func changeGameState(){
+        
+        if !monsterHappy {
+            
+            deaths = deaths + 1
+            if deaths == 1{
+                thirdSkullImg.alpha = OPAQUE
+            }
+            else if deaths == 2 {
+                secondSkullImg.alpha = OPAQUE
+            }
+            else
+            {
+                firstSkullImg.alpha = OPAQUE
+                gameOver()
+            }
+        }
+        
+        let random = arc4random_uniform(2)
+        
+        if random == 0{
+            foodImg.alpha = DIM_ALPHA
+            foodImg.userInteractionEnabled = false
+            
+            heartImg.alpha = OPAQUE
+            heartImg.userInteractionEnabled = true
+        }
+        else{
+            foodImg.alpha = OPAQUE
+            foodImg.userInteractionEnabled = true
+            
+            heartImg.alpha = DIM_ALPHA
+            heartImg.userInteractionEnabled = false
+        }
+        
+        currentItem = random
+        monsterHappy = false
+        
+    }
+    
+    func gameOver(){
+        monsterImg.playDeathAnimation()
+        
+        foodImg.alpha = DIM_ALPHA
+        foodImg.userInteractionEnabled = false
+        
+        heartImg.alpha = DIM_ALPHA
+        heartImg.userInteractionEnabled = false
+        timer.invalidate()
+    }
+    
     
     
     
